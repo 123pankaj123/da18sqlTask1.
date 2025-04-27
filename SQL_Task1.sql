af869116f89e1be1ce505837641f2efa8512a1bb
123pankaj123 <pankajmaharwade829@email.com>
2025-04-27 15:14:20 +0530

    Task1. completed

---
+++ SQL_Task1.sql
@@ -0,0 +1,300 @@
+create table sales(
+    id int primary key,
+	paint_type varchar,
+	price float,
+	lit int,
+	margin double PRECISION,
+	rating float,
+	coverage_sq_ft int,
+	sell_date date
+)
+select * from sales
+
+
+insert into sale (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values 
+(1,'spray',215,2,5,2.5,3,'4/21/2025')
+
+insert into sale (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values 
+(4,'oil',567,4,6,5,3,'4/21/2025')
+
+insert into sale (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values 
+(2,'oil',567,4,6,5,3,'4/21/2025')
+
+insert into sale (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values 
+(3,'oil',121,5,6,5,3,'4/21/2025')
+insert into sale (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values 
+(1,'spray',215,2,5,2.5,3,'4/21/2025')
+
+insert into sale (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values 
+(5,'plastic',121,5,6,5,3,'4/21/2025')
+
+insert into sale (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values 
+(6,'plastic',678,5,7,3, 5,'4/21/2025')
+
+insert into sale (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values 
+(8,'oil',567,4,6,5,3,'4/21/2025')
+
+insert into sale (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values 
+(7,'oil',567,4,6,5,3,'4/21/2025')
+
+ select * from sales
+
+insert into sales (id, paint_type,price,lit,rating,coverage_sq_ft,sell_date) 
+values 
+(9,'spray',25,10,2,3,'4/11/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values 
+(10,'spray',21,2,5,5,8,'03/11/2025')
+
+select * from sales
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values 
+(10,'spray',21,2,5,5,8,'03/11/2025')
+
+insert into sales (id, paint_type, price,margin,rating,coverage_sq_ft,sell_date) 
+values 
+(12,'spray',15,5,3, 18,'03/13/2025')
+
+insert into sales (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date) values (13,'spray',23,54,2,19,'03/14/2025')
+
+insert into sales (id,paint_type,price,margin,lit,coverage_sq_ft,sell_date) values (14,'spray',3,14,2,19,'03/15/2025')
+
+insert into sales (id,paint_type,price,margin,lit,sell_date) values (15,'spray',7,1,5,'03/15/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (16,'spray',215,2,5,2.5,3,'03/16/2025')
+
+insert into sales (id,paint_type,price,margin,lit,coverage_sq_ft,sell_date) values (17,'spray',6,1,2,9,'03/17/2025')
+
+
+insert into sales (id,paint_type,margin,lit,coverage_sq_ft,sell_date) values (18,'spray',1,12,9,'03/18/2025')
+
+select * from sales
+
+insert into sales (id,paint_type,margin,lit,coverage_sq_ft,sell_date) values (19,'acrylic',6,2,32,'03/19/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (20,'deco',20,2,5,5,16,'03/20/2025')
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft,sell_date) values (21,'spray',2,5,5,18,'03/21/2025')
+insert into sales (id, paint_type,price,margin,rating,coverage_sq_ft,sell_date) values (22,'exterior',25,65,7,9,'03/22/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,sell_date) values (23,'spray',215,2,5,3,'03/23/2025')
+
+insert into sales (id, price,lit,margin,rating,coverage_sq_ft,sell_date) values (24,25,2,5,2,3,'03/24/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (25,'oil',54,2,5,8,6,'03/25/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (26,'spray',15,2,5,89,43,'03/26/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (27,'deco',65,42,56,5,63,'03/27/2025')
+insert into sales (id, paint_type,price,lit,rating,coverage_sq_ft,sell_date) values (28,'spray',27,51,5,42,'03/28/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft) values (29,'acrylic',21,2,5,5,3)
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (30,'spray',25,2,5,2,5,'03/30/2025')
+insert into sales (id, price,lit,margin,rating,coverage_sq_ft,sell_date) values (31,2,72,58,26,38,'03/31/2025')
+
+select * from sales
+
+insert into sales (id, paint_type,price,lit,margin,rating,sell_date) values (32,'deco',65,27,2,5,'04/01/2025')
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft,sell_date) values (33,'spray',2,5,2,5,'04/02/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (34,'spray',5,72,9,76,89,'04/03/2025')
+
+insert into sales (id, paint_type,lit,margin,rating,coverage_sq_ft,sell_date) values (35,'spray',2,98,6,90,'04/04/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,sell_date) values (36,'plastic',2,9,7,4,'04/05/2025')
+insert into sales (id, price,lit,margin,rating,coverage_sq_ft,sell_date) values (37,5,72,69,7,44,'04/06/2025')
+
+insert into sales (id, paint_type,price,margin,rating,coverage_sq_ft,sell_date) values (38,'oil',5,9,56,9,'04/07/2025')
+insert into sales (id, paint_type,price) values (39,'spray',56)
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (40,'spray',5,72,9,76,89,'04/08/2025')
+
+insert into sales (id, paint_type,price,lit,coverage_sq_ft,sell_date) values (41,'deco',5,56,34,'04/09/2025')
+insert into sales (id, lit,margin,rating,coverage_sq_ft,sell_date) values (42,2,9,76,9,'04/10/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (43,'spray',5,72,9,76,89,'04/11/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (44,'acrylic',5,72,9,06,67,'04/12/2025')
+
+select * from sales
+
+insert into sales (id, paint_type,price,lit,margin,sell_date) values (45,'oil',49,9,33,'04/13/2025')
+
+insert into sales (id, paint_type,price,lit,rating,coverage_sq_ft,sell_date) values (46,'deco',5,72,9,76,'04/14/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (47,'exterior',5,72,9,76,89,'04/03/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,sell_date) values (48,'spray',54,2,19,79,'04/15/2025')
+insert into sales (id, lit,margin,rating,coverage_sq_ft,sell_date) values (49,5,7,9,6,'04/16/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (50,'spray',5,72,9,76,89,'04/17/2025')
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft,sell_date) values (51,'plastic',67,8,6,34,'04/18/2025')
+
+
+insert into sales (id, paint_type,price,lit,rating,coverage_sq_ft,sell_date) values (52,'deco',5,72,9,76,'04/19/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (53,'exterior',5,72,9,76,89,'04/20/2025')
+
+insert into sales (id, paint_type,price,lit,margin,sell_date) values (54,'spray',4,6,79,'04/21/2025')
+insert into sales (id, paint_type,price,rating,coverage_sq_ft,sell_date) values (55,5,7,9,6,'04/22/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,sell_date) values (56,'spray',5,42,5,9,'04/23/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (51,'plastic',NULL,8,6,34,'04/18/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (NULL,'interior',45,6,8,70,'04/19/2025')
+insert into sales (id, paint_type,price,lit,rating,coverage_sq_ft,sell_date) values (53,NULL,66,4,7,80,'04/20/2025')
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft,sell_date) values (54,'enamel',48,NULL,5,55,'04/21/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (55,'wood',NULL,5,8,66,'04/22/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (56,'exterior',59,3,9,70,'04/23/2025')
+insert into sales (id, paint_type,price,lit,margin,sell_date) values (57,'exterior',61,6,7,'04/24/2025')
+insert into sales (id, paint_type,price,lit,coverage_sq_ft,sell_date) values (58,'plastic',53,8,69,'04/25/2025')
+
+insert into sales (id, paint_type,lit,margin,coverage_sq_ft,sell_date) values (59,'deco',5,7,73,'04/26/2025')
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft) values (60,'spray',44,6,8,74)
+
+insert into sales (id, paint_type,price,margin,rating,coverage_sq_ft,sell_date) values (62,'spray',42,7,9,68,'04/27/2025')
+insert into sales (id, paint_type,price,lit,coverage_sq_ft,sell_date) values (62,'exterior',47,6,65,'04/28/2025')
+
+insert into sales (id, price,lit,rating,coverage_sq_ft,sell_date) values (63,46,5,7,60,'04/29/2025')
+insert into sales (id, paint_type,price,margin,rating,sell_date) values (64,'acrylic',3,9,58,'04/30/2025')
+
+insert into sales (id, paint_type,price,lit,coverage_sq_ft,sell_date) values (65,'plastic',52,5,8,'05/01/2025')
+insert into sales (id, paint_type,price,margin,rating,coverage_sq_ft,sell_date) values (66,'deco',41,4,7,55,'05/02/2025')
+
+select * from sales
+insert into sales (id, paint_type,lit,margin,coverage_sq_ft,sell_date) values (67,'spray',39,6,57,'05/03/2025')
+insert into sales (id, paint_type,lit,margin,rating,coverage_sq_ft,sell_date) values (68,'oil',48,2,8,54,'05/04/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,sell_date) values (69,'oil',6,7,4,52,'05/05/2025')
+insert into sales (id, paint_type,price,rating,coverage_sq_ft,sell_date) values (70,'oil',45,5,9,'05/06/2025')
+insert into sales (id, paint_type,lit,margin,rating,coverage_sq_ft,sell_date) values (71,'exterior',43,4,8,53,'05/07/2025')
+
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft,sell_date) values (72,'plastic',41,3,7,49,'05/08/2025')
+insert into sales (id, price,lit,margin,rating,coverage_sq_ft,sell_date) values (73,39,3,5,9,50,'05/09/2025')
+
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft,sell_date) values (61,'deco',37,2,7,47,'05/10/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,sell_date) values (74,'spray',35,11,8,48,'05/11/2025')
+
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft) values (75,'plastic',34,3,6,8,45)
+insert into sales (id, paint_type,price,lit,rating,sell_date) values (76,'spray',35,11,48,'05/11/2025')
+
+select * from sales
+
+insert into sales (id, paint_type,lit,margin,rating,coverage_sq_ft,sell_date) values (77,'deco',32,4,7,42,'05/13/2025')
+insert into sales (id, paint_type,price,margin,rating,coverage_sq_ft,sell_date) values (78,'plastic',31,4,6,40,'05/14/2025')
+
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft,sell_date) values (79,'oil',33,5,8,43,'05/15/2025')
+insert into sales (id, paint_type,price,lit,rating,sell_date) values (80,'exterior',30,6,7,'05/16/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (91,'deco',29,5,3,9,41,'05/17/2025')
+insert into sales (id, paint_type,price,lit,rating,coverage_sq_ft,sell_date) values (82,'spray',28,6,8,39,'05/18/2025')
+
+insert into sales (id, price,lit,margin,rating,coverage_sq_ft,sell_date) values (83,4,27,7,5,38,'05/19/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (84,'deco',3,8,4,5,36,'05/20/2025')
+
+insert into sales (id, paint_type,price,lit,coverage_sq_ft,sell_date) values (85,'plastic',25,2,9,'05/21/2025')
+insert into sales (id, paint_type,margin,rating,coverage_sq_ft,sell_date) values (86,'deco',24,6,34,'05/22/2025')
+
+insert into sales (id, paint_type,price,lit,coverage_sq_ft,sell_date) values (87,'spray',23,7,32,'05/23/2025')
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft,sell_date) values (88,'exterior',22,4,3,30,'05/24/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (89,'oil',21,3,6,8,29,'05/25/2025')
+
+insert into sales (id, price,lit,margin,rating,coverage_sq_ft,sell_date) values (90,20,2,7,7,27,'05/26/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft) values (81,'plastic',19,5,6,7,26)
+
+select * from sales
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft,sell_date) values (92,'acrylic',18,5,8,5,'05/28/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft) values (93,'spray',17,4,4,6,25)
+
+insert into sales (id, paint_type,lit,margin,rating,coverage_sq_ft,sell_date) values (94,'oil',16,3,9,24,'05/30/2025')
+insert into sales (id, paint_type,price,lit,coverage_sq_ft,sell_date) values (95,'exterior',15,6,22,'05/31/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (96,'plastic',14,6,5,7,21,'06/01/2025')
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft,sell_date) values (97,'deco',13,4,8,20,'06/02/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (98,'spray',12,3,9,5,6,'06/03/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,sell_date) values (99,'exterior',11,5,7,18,'06/04/2025')
+
+select * from sales
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (100,'oil',5,10,2,8,17,'06/05/2025')
+insert into sales (id, paint_type,lit,margin,rating,coverage_sq_ft,sell_date) values (101,'wood',9,5,6,16,'06/06/2025')
+insert into sales (id, paint_type,price,margin,rating,coverage_sq_ft,sell_date) values (102,'plastic',8,4,6,15,'06/07/2025')
+insert into sales (id, paint_type,price,lit,rating,coverage_sq_ft,sell_date) values (103,'spray',7,3,9,NULL,'06/08/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (104,'exterior',6,2,3,7,14,'06/09/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,sell_date) values (105,'deco',5,5,8,13,'06/10/2025')
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft,sell_date) values (106,'spray',6,5,8,11,'06/12/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,sell_date) values (107,'acrylic',6,6,8,11,'06/12/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (108,'spray',5,7,3,7,10,'06/13/2025')
+
+select * from sales
+insert into sales (id, price,lit,margin,rating,coverage_sq_ft,sell_date) values (109,4,6,5,6,9,'06/14/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft) values (110,'exterior',34,2,9,8,7)
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (112,3,3,5,6,7,7,'06/16/2025')
+insert into sales (id, paint_type,lit,margin,rating,coverage_sq_ft,sell_date) values (111,'spray',2,34,8,6,'06/17/2025')
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft,sell_date) values (113,'plastic',1,3,6,5,'06/18/2025')
+insert into sales (id, price,margin,rating,coverage_sq_ft,sell_date) values (114,4,2,7,5,'06/19/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (115,'deco',3,5,4,8,4,'06/20/2025')
+insert into sales (id, paint_type,price,lit,rating,coverage_sq_ft,sell_date) values (116,'plastic',4,5,6,3,'06/21/2025')
+insert into sales (id, paint_type,price,margin,rating,coverage_sq_ft,sell_date) values (117,'exterior',5,2,9,2,'06/22/2025')
+
+select * from  sales
+
+insert into sales (id, paint_type,price,lit,margin,rating,sell_date) values (118,'oil',6,3,7,6,'06/23/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (119,'spray',7,4,55,9,5,'06/24/2025')
+
+insert into sales (id, price,lit,margin,rating,coverage_sq_ft,sell_date) values (120,8,5,8,8,6,'06/25/2025')
+insert into sales (id, paint_type,pric,lit,margin,rating,coverage_sq_ft,sell_date) values (121,'deco',9,5,6,7,'06/26/2025')
+insert into sales (id, paint_type,margin,rating,coverage_sq_ft,sell_date) values (122,'exterior',2,7,8,'06/27/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (123,'plastic',11,3,98,9,9,'06/28/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (121,'spray',12,45,8,6,10,'06/29/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (125,'wood',13,5,6,NULL,'06/30/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (126,4,5,14,4,7,11,'07/01/2025')
+
+select * from sales
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (127,'deco',15,4,66,8,12,'07/02/2025')
+insert into sales (id, paint_type,price,lit,margin,coverage_sq_ft,sell_date) values (128,'spray',16,3,9,13,'07/03/2025')
+insert into sales (id, paint_type,price,margin,rating,coverage_sq_ft,sell_date) values (129,'oil',17,4,7,56,'07/04/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (130,'plastic',18,5,7,6,14,'07/05/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,sell_date) values (133,'exterior',19,56,8,15,'07/06/2025')
+
+insert into sales (id, price,lit,margin,coverage_sq_ft,sell_date) values (134,56,20,2,16,'07/07/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (135,'spray',21,3,4,6,6,'07/10/2025')
+
+insert into sales (id, paint_type,price,lit,margin,rating,sell_date) values (136,'spray',21,4,5,6,'07/10/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (137,'spray',21,3,4,6,6,'07/10/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (138,'oil',1,7,4,89,6,'07/10/2025')
+ 
+select *from sales
+
+insert into sales (id, paint_type,price,margin,rating,coverage_sq_ft,sell_date) values (139,'spray',21,93,65,6,'07/11/2025')
+
+insert into sales (id, paint_type,price,lit,margin,sell_date) values (141,'spray',28,2,22,'07/15/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (141,'oil',29,5,7,23,'07/16/2025')
+insert into sales (id, paint_type,price,lit,rating,coverage_sq_ft,sell_date) values (142,'deco',30,6,9,24,'07/17/2025')
+
+select * from sales
+
+insert into sales (id, paint_type,lit,margin,rating,coverage_sq_ft,sell_date) values (143,'exterior',31,4,8,65,'07/18/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (144,'plastic',32,3,6,7,25,'07/19/2025')
+insert into sales (id, paint_type,price,margin,rating,coverage_sq_ft,sell_date) values (145,'spray',33,2,6,26,'07/20/2025')
+insert into sales (id, paint_type,lit,margin,rating,coverage_sq_ft,sell_date) values (146,'oil',34,5,5,27,'07/21/2025')
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (147,45,6,35,4,9,28,'07/22/2025')
+insert into sales (id, paint_type,price,lit,rating,coverage_sq_ft,sell_date) values (148,'plastic',36,6,8,29,'07/23/2025')
+
+select * from sales
+
+insert into sales (id, paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) values (149,'deco',45,35,4,9,28,'07/24/2025')
+insert into sales (id, paint_type,price,lit,rating,coverage_sq_ft,sell_date) values (150,'plastic',6,6,89,89,'07/25/2025')
+
+select * from sales
